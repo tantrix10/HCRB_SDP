ec2698b7685da8ce1f37ee39d2d822074fbda2bd
Jamie Friel <jamiefriel@googlemail.com>
2021-06-17 23:57:45 +0100

    ran through black and adding some docstrings

--- SPD/HolPure.py
+++ SPD/HolPure.py
@@ -1,74 +1,76 @@
-import qutip     as qt
-import numpy     as np
-import cvxpy     as cp
+import qutip as qt
+import numpy as np
+import cvxpy as cp
 
 
-def naghol_sdp(phi, dphi, d, solve = 'MOSEK', verbose_state = False):
+def naghol_sdp(phi, dphi, d, solve="MOSEK", verbose_state=False):
     """
     args:
     -phi : qutip quantum object, pure normalised state, of size (d, 1)
     -dphi: qutip quantum object, vector of derivatives, of size (d, 1) x npar, where npar is the number of parameters
     -d   : integer             , dimension of Hilbert space
-    
+
     default args:
-    -solver        : string, string of solver, default is MOSEK, can also be CVXOPT, SCS. More details: https://www.cvxpy.org/tutorial/advanced/index.html 
-    -verbose_state : bool  , option if you want the cvxpy solver to turn maximal information or not. verbose_state = true prints max info to terminal       
+    -solver        : string, string of solver, default is MOSEK, can also be CVXOPT, SCS. More details: https://www.cvxpy.org/tutorial/advanced/index.html
+    -verbose_state : bool  , option if you want the cvxpy solver to turn maximal information or not. verbose_state = true prints max info to terminal
 
     returns: float, Holevo-Cramer-Rao bound of the statistical model defined by the encoded state and derivatives
 
     """
-    #dictionary of SDP solvers
-    solver_options = {'MOSEK':cp.MOSEK, 'CVXOPT': cp.CVXOPT, 'SCS':cp.SCS }
+    # dictionary of SDP solvers
+    solver_options = {"MOSEK": cp.MOSEK, "CVXOPT": cp.CVXOPT, "SCS": cp.SCS}
 
-    #define the number of parameters
+    # define the number of parameters
     npar = len(dphi)
 
-    #here we define the matrix of horizontal lifts-Lmat
-    #The horizontal lift is defined as: 2(|\partial_i \phi>  − <\phi| \partial_i \phi>|\phi> ) 
-    psidpsi = phi.dag()*dphi
+    # here we define the matrix of horizontal lifts-Lmat
+    # The horizontal lift is defined as: 2(|\partial_i \phi>  − <\phi| \partial_i \phi>|\phi> )
+    psidpsi = phi.dag() * dphi
     pardphi = phi * psidpsi
 
-    Lmat    = 2 * (dphi-pardphi)
+    Lmat = 2 * (dphi - pardphi)
 
-    #convert quantum objects to numpy arrays
-    #n.b. this should be sped up by just calling phi.full(), will do this and test asap
-    psi = np.zeros(d,dtype= complex)
+    # convert quantum objects to numpy arrays
+    # n.b. this should be sped up by just calling phi.full(), will do this and test asap
+    psi = np.zeros(d, dtype=complex)
     for i in range(d):
         psi[i] = phi[i][0][0]
 
-    #same here as above with horizontal lifts
-    Lmatt = np.zeros((d,npar),dtype = complex)
+    # same here as above with horizontal lifts
+    Lmatt = np.zeros((d, npar), dtype=complex)
     for i in range(d):
         for j in range(npar):
-            Lmatt[i,j] = Lmat[j][i] 
+            Lmatt[i, j] = Lmat[j][i]
 
-    #set up SDP as defined in the readme/paper
+    # set up SDP as defined in the readme/paper
 
-    #set up variables 'variance matrix' V and 'derivative variable matrices' X
-    V = cp.Variable((npar,npar),PSD =True)
-    X = cp.Variable((d,npar),complex = True)
+    # set up variables 'variance matrix' V and 'derivative variable matrices' X
+    V = cp.Variable((npar, npar), PSD=True)
+    X = cp.Variable((d, npar), complex=True)
 
-    #set up SD-matrix to optimise
-    A = cp.vstack([ cp.hstack([V , X.H ]) , cp.hstack([X , np.identity(d) ])  ])
+    # set up SD-matrix to optimise
+    A = cp.vstack([cp.hstack([V, X.H]), cp.hstack([X, np.identity(d)])])
 
+    # constraints need to be split because cvxpy will only accept a real valued SD-matrix
+    constraints = [
+        cp.vstack(
+            [cp.hstack([cp.real(A), -cp.imag(A)]), cp.hstack([cp.imag(A), cp.real(A)])]
+        )
+        >> 0,
+        cp.real(X.H @ Lmatt) == np.identity(3),
+        ((psi.transpose()).conjugate() @ X) == 0,
+    ]
 
-    #constraints need to be split because cvxpy will only accept a real valued SD-matrix
-    constraints = [ cp.vstack([   cp.hstack([cp.real(A),-cp.imag(A)]), cp.hstack([cp.imag(A),cp.real(A)])   ]) >> 0, 
-                    cp.real(X.H @ Lmatt) == np.identity(3),
-                     ((psi.transpose()).conjugate() @ X) == 0]
-
-    #define objective function
+    # define objective function
     obj = cp.Minimize(cp.trace(V))
 
-    #define problem
-    prob = cp.Problem(obj,constraints)
+    # define problem
+    prob = cp.Problem(obj, constraints)
 
-    #solve problem
-    prob.solve(solver = solver_options[solve], verbose = verbose_state)
-    
-    #extract and return value
+    # solve problem
+    prob.solve(solver=solver_options[solve], verbose=verbose_state)
+
+    # extract and return value
     out = prob.value
 
     return out
-
-
--- SPD/HolSDP.py
+++ SPD/HolSDP.py
@@ -1,10 +1,12 @@
-import qutip     as qt
-import numpy     as np
-import cvxpy     as cp
-import scipy     as sci
+import qutip as qt
+import numpy as np
+import cvxpy as cp
+import scipy as sci
+
+# TODO: make the imports above explicit imports, not star imports
 
 
-def rank(D, tol = 1e-9):
+def rank(D, tol=1e-9):
     """
     args:
     -D: numpy array, Eigenvalues of rho
@@ -24,118 +26,227 @@ def rank(D, tol = 1e-9):
     return Dnonzero, rank
 
 
-
 def SmatRank(snonzero, d, rnk, dim):
-    #print(snonzero,d)
-    mask    = np.triu(np.ones([rnk], dtype= bool),1)
-    scols = np.zeros((rnk,rnk))
+    """
+    S matrix, that is the matrix that represents the inner product induced
+    by rho on the space of linear operators on the Hilbert space.
+
+    More details in equ 9 in the paper linked in HCRB_sdp.
+    """
+    mask = np.triu(np.ones([rnk], dtype=bool), 1)
+    scols = np.zeros((rnk, rnk))
     for i in range(rnk):
         for j in range(rnk):
-            scols[i,j] = np.real(snonzero[j])
+            scols[i, j] = np.real(snonzero[j])
 
-    srows   = scols.transpose()
+    srows = scols.transpose()
     siminsj = -srows + scols
-    siplsj  = scols + srows
+    siplsj = scols + srows
 
-    diagS   = np.concatenate((snonzero.transpose(),siplsj[mask].transpose(),siplsj[mask].transpose(),np.matlib.repmat(snonzero.transpose(),2*(d-rnk),1).flatten() ))
+    diagS = np.concatenate(
+        (
+            snonzero.transpose(),
+            siplsj[mask].transpose(),
+            siplsj[mask].transpose(),
+            np.matlib.repmat(snonzero.transpose(), 2 * (d - rnk), 1).flatten(),
+        )
+    )
 
-    Smat = sci.sparse.spdiags(diagS,0,dim,dim)
+    Smat = sci.sparse.spdiags(diagS, 0, dim, dim)
     Smat = sci.sparse.csr_matrix(Smat)
     Smat = Smat.todense()
-    Smat = np.matrix(Smat, dtype = complex)
+    Smat = np.matrix(Smat, dtype=complex)
 
     if rnk != 1:
-        offdRank = 1j* sci.sparse.spdiags(siminsj[mask],0,int((rnk**2-rnk)/2),int((rnk**2-rnk)/2)).todense()
+        offdRank = (
+            1j
+            * sci.sparse.spdiags(
+                siminsj[mask], 0, int((rnk ** 2 - rnk) / 2), int((rnk ** 2 - rnk) / 2)
+            ).todense()
+        )
     else:
         offdRank = 0
 
-    offdKer = -1j*sci.sparse.spdiags(np.matlib.repmat(snonzero,d-rnk,1).flatten(),0,rnk*(d-rnk),rnk*(d-rnk)).todense()   
+    offdKer = (
+        -1j
+        * sci.sparse.spdiags(
+            np.matlib.repmat(snonzero, d - rnk, 1).flatten(),
+            0,
+            rnk * (d - rnk),
+            rnk * (d - rnk),
+        ).todense()
+    )
 
-
-    Smat[int(rnk+(rnk**2-rnk)/2):int(rnk+(rnk**2-rnk))                        ,int(rnk):int(rnk+(rnk**2-rnk)/2)]                                      =-offdRank
-    Smat[int(rnk):int(rnk+(rnk**2-rnk)/2)                                     ,int(rnk+(rnk**2-rnk)/2):int(rnk+(rnk**2-rnk))]                         = offdRank
-    Smat[int(rnk+(rnk**2-rnk)+rnk*(d-rnk)):int(rnk+(rnk**2-rnk)+2*rnk*(d-rnk)),int(rnk+(rnk**2-rnk)):int(rnk+(rnk**2-rnk)+rnk*(d-rnk))]               =-offdKer
-    Smat[int(rnk+(rnk**2-rnk)):int(rnk+(rnk**2-rnk)+rnk*(d-rnk))              ,int(rnk+(rnk**2-rnk)+rnk*(d-rnk)):int(rnk+(rnk**2-rnk)+2*rnk*(d-rnk))] = offdKer
+    Smat[
+        int(rnk + (rnk ** 2 - rnk) / 2) : int(rnk + (rnk ** 2 - rnk)),
+        int(rnk) : int(rnk + (rnk ** 2 - rnk) / 2),
+    ] = -offdRank
+    Smat[
+        int(rnk) : int(rnk + (rnk ** 2 - rnk) / 2),
+        int(rnk + (rnk ** 2 - rnk) / 2) : int(rnk + (rnk ** 2 - rnk)),
+    ] = offdRank
+    Smat[
+        int(rnk + (rnk ** 2 - rnk) + rnk * (d - rnk)) : int(
+            rnk + (rnk ** 2 - rnk) + 2 * rnk * (d - rnk)
+        ),
+        int(rnk + (rnk ** 2 - rnk)) : int(rnk + (rnk ** 2 - rnk) + rnk * (d - rnk)),
+    ] = -offdKer
+    Smat[
+        int(rnk + (rnk ** 2 - rnk)) : int(rnk + (rnk ** 2 - rnk) + rnk * (d - rnk)),
+        int(rnk + (rnk ** 2 - rnk) + rnk * (d - rnk)) : int(
+            rnk + (rnk ** 2 - rnk) + 2 * rnk * (d - rnk)
+        ),
+    ] = offdKer
 
     return Smat
 
 
+def Rmat(S, tol=1e-8):
+    """
+    Take the square-root of the S-matrix, reduced to the rank of the S-matrix.
+    """
 
-def Rmat(S, tol = 1e-8):
-    
-    d,v    = np.linalg.eig(S)
-    
-    ind    = d.argsort()[::-1]
-    v      = v[:,ind]
-    d      = d[ind]
-    rank   = sum([d[i]>tol for i in range(len(d))])
+    d, v = np.linalg.eig(S)
+
+    ind = d.argsort()[::-1]
+    v = v[:, ind]
+    d = d[ind]
+    rank = sum([d[i] > tol for i in range(len(d))])
     dclean = d[:rank]
-    vclean = v[:,:rank]
-    dout   = np.diag(dclean)
+    vclean = v[:, :rank]
+    dout = np.diag(dclean)
 
-    return np.sqrt(dout)@ vclean.transpose().conjugate()
-
-
+    return np.sqrt(dout) @ vclean.transpose().conjugate()
 
 
-def naghol_sdp(rho, drho, d, solve = 'MOSEK', verbose_state = False)):
-    rho = (rho.dag()+rho)/2
-    npar = 3
- 
+def HCRB_sdp(rho, drho, solve="MOSEK", verbose_state=False):
+    """
+    Calculate the HCRB of the statistical model define by rho and its derivatives.
+
+    The derivation of this formulation of the HCRB and more details can be found in the paper:
+
+    - [published](https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.123.200503)
+    - [arxiv](https://arxiv.org/abs/1906.05724)
+
+    This current implementation currently assumes QuTip Qobjs for rho and drho.
+    # TODO: tweak the code to take qobj or numpy arrays.
+
+    Inputs:
+
+    - rho: Qobj, parameter encoded state
+    - drho: list[Qobj], dervatives of rho in the direction of the paramaters of interest
+    - solve, SDP solver options: "MOSEK", "SCS" and "CVXOPT" [more details](https://www.cvxpy.org/tutorial/advanced/index.html)
+    - verbose_state, if True then switches the SDP solver to verbose mode
+
+    returns:
+
+    The HRCB (float) for the statistical model defined by the state and its derivatives.
+
+    """
+
+    # The SDP calculation is very sensitive to the input state being exactly hermitian.
+    rho = (rho.dag() + rho) / 2
+
+    d = rho.dims[0][0]
+    npar = len(drho)
+
     D, Vi = np.linalg.eigh(rho.full())
 
     D = np.real(D)
 
-    Vi = Vi[:,::-1]
+    Vi = Vi[:, ::-1]
     D = D[::-1]
-    
-    Vi = qt.Qobj(Vi,dims= [[2]*n,[2]*n])
+
+    Vi = qt.Qobj(Vi, dims=[[2] * n, [2] * n])
 
     snonzero, rnk = rank(D)
 
-    solver_options = {'MOSEK':cp.MOSEK, 'CVXOPT': cp.CVXOPT, 'SCS':cp.SCS }
+    solver_options = {"MOSEK": cp.MOSEK, "CVXOPT": cp.CVXOPT, "SCS": cp.SCS}
 
-    maskDiag = np.diag(np.ndarray.flatten(np.concatenate((np.ones([rnk,1],dtype = bool),np.zeros([d-rnk,1],dtype = bool)))))
-    maskRank = np.concatenate((np.concatenate((np.triu(np.ones(rnk,dtype = bool),1), np.zeros([rnk,d-rnk],dtype = bool)),axis = 1),np.zeros([d-rnk,d],dtype = bool)))
-    maskKern = np.concatenate((np.concatenate((np.zeros([rnk,rnk],dtype = bool),np.ones([rnk,d-rnk],dtype = bool)),axis = 1),np.zeros([d-rnk,d],dtype = bool)))
+    maskDiag = np.diag(
+        np.ndarray.flatten(
+            np.concatenate(
+                (np.ones([rnk, 1], dtype=bool), np.zeros([d - rnk, 1], dtype=bool))
+            )
+        )
+    )
+    maskRank = np.concatenate(
+        (
+            np.concatenate(
+                (
+                    np.triu(np.ones(rnk, dtype=bool), 1),
+                    np.zeros([rnk, d - rnk], dtype=bool),
+                ),
+                axis=1,
+            ),
+            np.zeros([d - rnk, d], dtype=bool),
+        )
+    )
+    maskKern = np.concatenate(
+        (
+            np.concatenate(
+                (np.zeros([rnk, rnk], dtype=bool), np.ones([rnk, d - rnk], dtype=bool)),
+                axis=1,
+            ),
+            np.zeros([d - rnk, d], dtype=bool),
+        )
+    )
 
-    fulldim = 2*rnk*d-rnk**2
+    fulldim = 2 * rnk * d - rnk ** 2
 
-    drhomat = np.zeros((fulldim,npar),dtype=np.complex_)
+    drhomat = np.zeros((fulldim, npar), dtype=np.complex_)
 
     for i in range(npar):
-        drho[i] = (drho[i].dag()+drho[i])/2
-        eigdrho = (Vi.dag())*drho[i]*Vi
+        drho[i] = (drho[i].dag() + drho[i]) / 2
+        eigdrho = (Vi.dag()) * drho[i] * Vi
         eigdrho = eigdrho.full()
         ak = eigdrho[maskKern]
-        ak = ak.reshape((rnk,d-rnk)).transpose()
-        ak = ak.reshape((rnk*(d-rnk)))
+        ak = ak.reshape((rnk, d - rnk)).transpose()
+        ak = ak.reshape((rnk * (d - rnk)))
 
- 
-        row = np.concatenate((eigdrho[maskDiag],np.real(eigdrho[maskRank]),np.imag(eigdrho[maskRank]),np.real(ak),np.imag(ak)))
-        drhomat[:,i] =  row
+        row = np.concatenate(
+            (
+                eigdrho[maskDiag],
+                np.real(eigdrho[maskRank]),
+                np.imag(eigdrho[maskRank]),
+                np.real(ak),
+                np.imag(ak),
+            )
+        )
+        drhomat[:, i] = row
 
-    S   = SmatRank(snonzero,d, rnk, fulldim)
-    S   = (S.transpose().conjugate()+S)/2
+    S = SmatRank(snonzero, d, rnk, fulldim)
+    S = (S.transpose().conjugate() + S) / 2
 
     R = Rmat(S)
-    
+
     effdim = R.shape[0]
-    idd = np.diag(np.ndarray.flatten(np.concatenate((np.ones((rnk)),2*np.ones((fulldim-rnk))))))
+    idd = np.diag(
+        np.ndarray.flatten(
+            np.concatenate((np.ones((rnk)), 2 * np.ones((fulldim - rnk))))
+        )
+    )
 
-    V = cp.Variable((npar,npar),PSD =True)
-    X = cp.Variable((fulldim,npar))
+    V = cp.Variable((npar, npar), PSD=True)
+    X = cp.Variable((fulldim, npar))
 
-    A = cp.vstack([ cp.hstack([V , X.T @ R.conjugate().transpose()]) , cp.hstack([R @ X , np.identity(effdim) ])])
+    A = cp.vstack(
+        [
+            cp.hstack([V, X.T @ R.conjugate().transpose()]),
+            cp.hstack([R @ X, np.identity(effdim)]),
+        ]
+    )
 
-    constraints = [ cp.vstack([   cp.hstack([cp.real(A),-cp.imag(A)]), cp.hstack([cp.imag(A),cp.real(A)])   ]) >> 0,  
-                                 X.T @ idd @ drhomat == np.identity(3)]
+    constraints = [
+        cp.vstack(
+            [cp.hstack([cp.real(A), -cp.imag(A)]), cp.hstack([cp.imag(A), cp.real(A)])]
+        )
+        >> 0,
+        X.T @ idd @ drhomat == np.identity(3),
+    ]
 
     obj = cp.Minimize(cp.trace(V))
-    prob = cp.Problem(obj,constraints)
-    prob.solve(solver = solver_options.get(solve, cp.SCS), verbose = verbose_state)
+    prob = cp.Problem(obj, constraints)
+    prob.solve(solver=solver_options.get(solve, cp.SCS), verbose=verbose_state)
     out = prob.value
     return out
-
-
-
